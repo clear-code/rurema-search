e36e931f3ba960e00d0bcc8d0db13d8e2339d19f
Kouhei Sutou <kou@clear-code.com>
2010-04-18 17:40:12 +0900

    genereate static HTML.

--- update.sh
+++ update.sh
@@ -3,16 +3,17 @@
 for version in 1.8.7 1.8.8 1.9.1 1.9.2; do
     ruby \
 	-I ~/work/ruby/bitclust/lib \
-	-I ~/work/ruby/rroonga/lib \
-	-I ~/work/ruby/rroonga/ext \
 	~/work/ruby/bitclust/bin/bitclust.rb \
 	--database ~/work/ruby/rurema-search/db-${version} \
 	init encoding=euc-jp version=${version}
     ruby \
 	-I ~/work/ruby/bitclust/lib \
-	-I ~/work/ruby/rroonga/lib \
-	-I ~/work/ruby/rroonga/ext \
 	~/work/ruby/bitclust/bin/bitclust.rb \
 	--database ~/work/ruby/rurema-search/db-${version} \
 	update --stdlibtree ~/work/ruby/rubydoc/refm/api/src
+    ruby \
+	-I ~/work/ruby/bitclust/lib \
+	~/work/ruby/bitclust/tools/bc-tohtmlpackage.rb \
+	--database ~/work/ruby/rurema-search/db-${version} \
+	--outputdir ~/work/ruby/rurema-search/public/${version}
 done
