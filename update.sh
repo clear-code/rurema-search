11bc5ff4cb05b23183177d06da830822d6f2d9ce
Kouhei Sutou <kou@clear-code.com>
2010-04-19 22:27:05 +0900

    use bin/bitclust-generate-static-html.

--- update.sh
+++ update.sh
@@ -6,19 +6,30 @@ rubydoc_dir=${base_dir}/../rubydoc
 
 for version in 1.8.7 1.8.8 1.9.1 1.9.2; do
     ruby \
-	-I ${bitclust_dir}/lib \
-	${bitclust_dir}/bin/bitclust.rb \
-	--database ${base_dir}/db-${version} \
-	init encoding=euc-jp version=${version}
+    	-I ${bitclust_dir}/lib \
+    	${bitclust_dir}/bin/bitclust.rb \
+    	--database ${base_dir}/db-${version} \
+    	init encoding=euc-jp version=${version}
     ruby \
-	-I ${bitclust_dir}/lib \
-	${bitclust_dir}/bin/bitclust.rb \
-	--database ${base_dir}/db-${version} \
-	update --stdlibtree ${rubydoc_dir}/refm/api/src
-    rm -rf ${base_dir}/public/${version}
+    	-I ${bitclust_dir}/lib \
+    	${bitclust_dir}/bin/bitclust.rb \
+    	--database ${base_dir}/db-${version} \
+    	update --stdlibtree ${rubydoc_dir}/refm/api/src
+    rm -rf ${base_dir}/public/${version}.{old,new}
     ruby \
+	-I ${base_dir}/lib \
 	-I ${bitclust_dir}/lib \
+	${base_dir}/bin/bitclust-generate-static-html \
 	${bitclust_dir}/tools/bc-tohtmlpackage.rb \
 	--database ${base_dir}/db-${version} \
-	--outputdir ${base_dir}/public/${version}
+	--outputdir ${base_dir}/public/${version}.new
+    mv ${base_dir}/public/${version}{,.old}
+    mv ${base_dir}/public/${version}{.new,}
+    rm -rf ${base_dir}/public/${version}.old
 done
+
+ruby1.9.1 \
+    -I../rroonga.19/lib \
+    -I../rroonga.19/ext/groonga \
+    ${base_dir}/bin/bitclust-indexer \
+    ${base_dir}/db-*
