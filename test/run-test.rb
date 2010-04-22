2b8f26ea67bfc56346372e2226c2a48d1f2b8e76
Kouhei Sutou <kou@clear-code.com>
2010-04-23 00:08:17 +0900

    add a test but not work yet.

---
+++ test/run-test.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+$VERBOSE = true
+
+$KCODE = "u" if RUBY_VERSION < "1.9"
+
+base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+test_unit_dir = File.join(base_dir, "test-unit")
+test_unit_lib_dir = File.join(test_unit_dir, "lib")
+bitclust_dir = File.expand_path(File.join(base_dir, "..", "bitclust"))
+bitclust_lib_dir = File.join(bitclust_dir, "lib")
+lib_dir = File.join(base_dir, "lib")
+test_dir = File.join(base_dir, "test")
+
+unless File.exist?(test_unit_dir)
+  test_unit_repository = "http://test-unit.rubyforge.org/svn/trunk/"
+  system("svn co #{test_unit_repository} #{test_unit_dir}") or exit(false)
+end
+
+$LOAD_PATH.unshift(test_unit_lib_dir)
+
+require 'test/unit'
+
+ARGV.unshift("--priority-mode")
+ARGV.unshift(File.join(test_dir, "test-unit.yml"))
+ARGV.unshift("--config")
+
+$LOAD_PATH.unshift(bitclust_lib_dir)
+
+$LOAD_PATH.unshift(lib_dir)
+
+$LOAD_PATH.unshift(test_dir)
+require 'rurema-search-test-utils'
+
+exit Test::Unit::AutoRunner.run(true, test_dir)
---
+++ test/rurema-search-test-utils.rb
@@ -0,0 +1,101 @@
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'webrick/httpstatus'
+
+require 'rubygems'
+require 'rack'
+
+require 'rurema_search'
+require 'rurema_search/groonga_searcher'
+
+
+require 'rack/test'
+require 'webrat'
+
+Webrat.configure do |config|
+  config.mode = :rack
+end
+
+module RuremaSearchTestUtils
+  include Rack::Test::Methods
+  include Webrat::Methods
+  include Webrat::Matchers
+
+  class << self
+    def database
+      @database ||= ensure_database
+    end
+
+    private
+    def ensure_database
+      database_dir = test_dir + "groonga-database"
+      database_file = database_dir + "bitclust.db"
+      dump_file = fixtures_dir + "dump.grn"
+      if !database_file.exist? or database_file.mtime < dump_file.mtime
+        FileUtils.rm_rf(database_dir.to_s)
+        database_dir.mkpath
+        command = "groonga -n #{database_file} < #{dump_file}"
+        result = `#{command} 2>&1`
+        unless $?.success?
+          FileUtils.rm_rf(database_dir.to_s)
+          raise "failed to create test database: " +
+                  "<#{command}>: <#{$?.to_i}>: <#{result}>"
+        end
+      end
+      _database = RuremaSearch::GroongaDatabase.new
+      _database.open(database_dir.to_s, "utf-8")
+      _database
+    end
+  end
+
+  private
+  def assert_response(code)
+    assert_equal(resolve_status(code), resolve_status(webrat.response_code))
+  end
+
+  def resolve_status(code_or_message)
+    messages = WEBrick::HTTPStatus::StatusMessage
+    if code_or_message.is_a?(String)
+      message = code_or_message
+      [(messages.find {|key, value| value == message} || [])[0],
+       message]
+    else
+      code = code_or_message
+      [code, messages[code]]
+    end
+  end
+
+  def database
+    RuremaSearchTestUtils.database
+  end
+
+  module_function
+  def test_dir
+    @test_dir ||= Pathname(__FILE__).dirname
+  end
+
+  def base_dir
+    @base_dir ||= test_dir.parent
+  end
+
+  def tmp_dir
+    @tmp_dir ||= test_dir + "tmp"
+  end
+
+  def fixtures_dir
+    @fixtures_dir ||= test_dir + "fixtures"
+  end
+end
