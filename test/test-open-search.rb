070d482a6618eaaac8b3cb93d24be99ff77950a4
Kouhei Sutou <kou@clear-code.com>
2010-05-05 08:42:58 +0900

    add OpenSearch description test.

---
+++ test/test-open-search.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class OpenSearchTest < Test::Unit::TestCase
+  include RuremaSearchTestUtils
+  include ERB::Util
+
+  def test_top_level_open_search_description
+    visit "/open_search_description.xml"
+    assert_open_search_description("http://#{host}/")
+  end
+
+  def test_versioned_open_search_description
+    visit "/version:1.8.8/open_search_description.xml"
+    assert_open_search_description("http://#{host}/version:1.8.8/")
+  end
+
+  private
+  def assert_open_search_description(expected_template)
+    assert_equal("application/opensearchdescription+xml",
+                 webrat.response.content_type)
+    xml = Nokogiri::XML(response_body)
+    url = xml.xpath("//node()[name()='Url']")[0]
+    assert_equal(expected_template, url["template"])
+  end
+end
