eb83e2fa9647a7ac80f3eb11f2d63dfc9a7eebe1
Kouhei Sutou <kou@clear-code.com>
2010-06-22 10:29:14 +0900

    support --no-rurema-update and --no-index-update options.

--- update.sh
+++ update.sh
@@ -4,45 +4,62 @@ base_dir=$(dirname $0)
 bitclust_dir=${base_dir}/../bitclust
 rubydoc_dir=${base_dir}/../rubydoc
 
-svn up -q ${rubydoc_dir}
-
-for version in 1.8.7 1.8.8 1.9.1 1.9.2; do
-    ruby \
-    	-I ${bitclust_dir}/lib \
-    	${bitclust_dir}/bin/bitclust.rb \
-    	--database ${base_dir}/db-${version} \
-    	init encoding=euc-jp version=${version}
-    ruby \
-    	-I ${bitclust_dir}/lib \
-    	${bitclust_dir}/bin/bitclust.rb \
-    	--database ${base_dir}/db-${version} \
-    	update --stdlibtree ${rubydoc_dir}/refm/api/src
-    ruby \
-    	-I ${bitclust_dir}/lib \
-    	${bitclust_dir}/bin/bitclust.rb \
-    	--database ${base_dir}/db-${version} \
-	--capi \
-    	update ${rubydoc_dir}/refm/capi/src/**/*.rd
-    rm -rf ${base_dir}/public/${version}.{old,new}
-    ruby \
-	-I ${base_dir}/lib \
-	-I ${bitclust_dir}/lib \
-	${base_dir}/bin/bitclust-generate-static-html \
-	${bitclust_dir}/tools/bc-tohtmlpackage.rb \
-	--quiet \
-	--fs-casesensitive \
-	--database ${base_dir}/db-${version} \
-	--outputdir ${base_dir}/public/${version}.new
-    mv ${base_dir}/public/${version}{,.old}
-    mv ${base_dir}/public/${version}{.new,}
-    rm -rf ${base_dir}/public/${version}.old
+rurema_update=yes
+index_update=yes
+for argument in $*; do
+    case "$argument" in
+	"--no-rurema-update")
+	    rurema_update=no
+	    ;;
+	"--no-index-update")
+	    index_update=no
+	    ;;
+    esac
 done
 
-rm -rf ${base_dir}/groonga-database
-(sleep 5; touch ${base_dir}/tmp/restart.txt) &
+if [ "$rurema_update" = "yes" ]; then
+    svn up -q ${rubydoc_dir}
 
-ruby1.9.1 \
-    -I${base_dir}/../rroonga/lib \
-    -I${base_dir}/../rroonga/ext/groonga \
-    ${base_dir}/bin/bitclust-indexer \
-    ${base_dir}/db-*
+    for version in 1.8.7 1.8.8 1.9.1 1.9.2; do
+	ruby \
+	    -I ${bitclust_dir}/lib \
+	    ${bitclust_dir}/bin/bitclust.rb \
+	    --database ${base_dir}/db-${version} \
+	    init encoding=euc-jp version=${version}
+	ruby \
+	    -I ${bitclust_dir}/lib \
+	    ${bitclust_dir}/bin/bitclust.rb \
+	    --database ${base_dir}/db-${version} \
+	    update --stdlibtree ${rubydoc_dir}/refm/api/src
+	ruby \
+	    -I ${bitclust_dir}/lib \
+	    ${bitclust_dir}/bin/bitclust.rb \
+	    --database ${base_dir}/db-${version} \
+	    --capi \
+	    update ${rubydoc_dir}/refm/capi/src/**/*.rd
+	rm -rf ${base_dir}/public/${version}.{old,new}
+	ruby \
+	    -I ${base_dir}/lib \
+	    -I ${bitclust_dir}/lib \
+	    ${base_dir}/bin/bitclust-generate-static-html \
+	    ${bitclust_dir}/tools/bc-tohtmlpackage.rb \
+	    --quiet \
+	    --fs-casesensitive \
+	    --database ${base_dir}/db-${version} \
+	    --outputdir ${base_dir}/public/${version}.new
+	mv ${base_dir}/public/${version}{,.old}
+	mv ${base_dir}/public/${version}{.new,}
+	rm -rf ${base_dir}/public/${version}.old
+    done
+fi
+
+if [ "$index_update" = "yes" ]; then
+    rm -rf ${base_dir}/groonga-database
+    (sleep 5; touch ${base_dir}/tmp/restart.txt) &
+
+    ruby1.9.1 \
+	-I${base_dir}/../rroonga/lib \
+	-I${base_dir}/../rroonga/ext/groonga \
+	${base_dir}/bin/bitclust-indexer \
+	${base_dir}/db-*
+fi
