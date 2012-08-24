a00fcb957643e89a6c3ada02d4346bb35f1f790e
Kouhei Sutou <kou@clear-code.com>
2012-08-24 20:25:45 +0900

    Disable Ruby 1.9.2

--- update.sh
+++ update.sh
@@ -75,7 +75,7 @@ update_rurema()
 if [ "$update_rurema" = "yes" ]; then
     svn up -q ${rubydoc_dir}
 
-    for version in 1.8.7 1.9.2 1.9.3; do
+    for version in 1.8.7 1.9.3; do
 	update_rurema $version
     done
     wait
