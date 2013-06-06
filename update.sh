d6c065dc2ccae489ae23d6014f6cf90ab43434e3
okkez <okkez000@gmail.com>
2013-06-07 00:17:06 +0900

    Use git instead of svn
    Because rurema repositories have been moved to GitHub.

--- update.sh
+++ update.sh
@@ -74,7 +74,7 @@ update_rurema()
 }
 
 if [ "$update_rurema" = "yes" ]; then
-    svn up -q ${rubydoc_dir}
+    (cd ${rubydoc_dir} && git pull --rebase)
 
     for version in 1.8.7 1.9.3 2.0.0; do
 	update_rurema $version
