c0e2a5330963098f93c281f5ac3cb025de02d874
Kouhei Sutou <kou@clear-code.com>
2013-07-14 20:29:59 +0900

    Use " (double quote) for string literal

--- test/rurema-search-test-utils.rb
+++ test/rurema-search-test-utils.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2010-2013  Kouhei Sutou <kou@clear-code.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,18 +13,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'open-uri'
-require 'webrick/httpstatus'
+require "open-uri"
+require "webrick/httpstatus"
 
-require 'rubygems'
-require 'rack'
-require 'time'
+require "rubygems"
+require "rack"
+require "time"
 
-require 'rurema_search'
-require 'rurema_search/groonga_searcher'
+require "rurema_search"
+require "rurema_search/groonga_searcher"
 
-require 'rack/test'
-require 'test/unit/capybara'
+require "rack/test"
+require "test/unit/capybara"
 
 module RuremaSearchTestUtils
   include Rack::Test::Methods
