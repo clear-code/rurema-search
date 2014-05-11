4ac18a96bfbbe3429c506e0ce07ba1ff19332601
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-05-11 22:42:46 +0900

    update ruby runtime

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :linked_files, %w{document.yaml production.yaml}
 set :linked_dirs, %w{groonga-database var/lib/suggest}
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.1'
+set :rbenv_ruby, '2.1.2'
 
 namespace :deploy do
   desc 'Restart application'
