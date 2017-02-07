ac1bc8a887f8bae31193ea011e5a685209c36717
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-02-07 11:33:08 +0900

    To use latest stable version

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :linked_dirs, %w{groonga-database var/lib/suggest}
 
 set :rbenv_type, :user
 set :rbenv_path, '/home/rurema/.rbenv'
-set :rbenv_ruby, '2.3.3'
+set :rbenv_ruby, '2.4.0'
 
 namespace :deploy do
   desc 'Restart application'
