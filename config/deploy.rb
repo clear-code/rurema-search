3b4dbf2816249e09b5faf264268dbea7dd60197f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-03-05 11:17:10 +0900

    Prepend snap path

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,10 @@ set :repo_url, 'https://github.com/ruby/rurema-search'
 set :branch, 'ro'
 set :deploy_to, '/var/rubydoc/rurema-search'
 
+set :default_env, {
+  'PATH' => '/snap/bin:$PATH',
+}
+
 set :linked_files, %w{document.yaml production.yaml}
 set :linked_dirs, %w{groonga-database var/lib/suggest}
 
