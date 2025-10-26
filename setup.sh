8d3672c181310742a29a637d4eb7a991c273d25e
Sutou Kouhei <kou@clear-code.com>
2025-10-26 16:21:57 +0900

    setup: use Bundler

--- setup.sh
+++ setup.sh
@@ -14,31 +14,11 @@ run()
 
 set -x
 
-run ${RUBY} -S gem install rack pkg-config
-
 run cd ${base_dir}/..
 run git clone https://github.com/rurema/bitclust.git bitclust
 run git clone https://github.com/rurema/doctree.git doctree
 
-run git clone https://github.com/groonga/groonga.git
-run cd groonga
-run ./autogen.sh
-run ./configure \
-    --prefix=${base_dir}/local \
-    --disable-document \
-    --disable-groonga-httpd
-run make
-run make install
-run cd -
-
-run git clone https://github.com/ranguba/rroonga.git
-run cd rroonga
-run export PKG_CONFIG_PATH=${base_dir}/local/lib/pkgconfig
-run ${RUBY} extconf.rb
-run make
-run cd -
-
-run git clone https://github.com/ranguba/racknga.git
-
 run cd rurema-search
-run ./update.sh
+
+run bundle install
+run bundle exec ./update.sh
